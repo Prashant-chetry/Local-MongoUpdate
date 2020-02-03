30c7309018460352cafcc83418bbc5430d82a03c
prashant.chetry <prashant.chetry@settlin.in>
2020-02-03 12:53:34 +0530

    create json file

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 const util = require('util')
-const url = process.argv[2] || '';
+const url = process.argv[3] || '';
 const fs = require('fs');
-// const Readable = require('stream').Readable;
-// const JSONStream = require('JSONStream')
+const JSONStream = require('JSONStream')
 const localUrl = 'mongodb://127.0.0.1:27017/';
 function main(url, localUrl) {
-	const name = process.argv[3] || '';
-	const collectionName = process.argv[4] || '';
-	const limit= parseInt(process.argv[5], 10) || 100;
-	const write = fs.createWriteStream(process.cwd() + '/data.txt');
-	MongoClient.connect(localUrl, {useUnifiedTopology: true}, (lerr, lClient)=> {
+	const opertaion = process.argv[2] || 'read';
+	const name = process.argv[4] || '';
+	const collectionName = process.argv[5] || '';
+	const limit= parseInt(process.argv[6], 10) || 100;
+	const options = process.argv[7] || 'local';
+	const write = fs.createWriteStream(process.cwd() + '/data.json');
+	if(opertaion === 'read'){
+			MongoClient.connect(localUrl, {useUnifiedTopology: true}, (lerr, lClient)=> {
 		if(lerr) console.log(lerr)
 		const lDb = lClient.db(name);
 		const lCollection =  lDb.collection(collectionName);
@@ -19,10 +21,16 @@ function main(url, localUrl) {
 		try{
 		const db = client.db(name);
 		const collection = db.collection(collectionName);
-		collection.find({}, {limit}).forEach(d=> {
-			console.log(d);
-			if(!lCollection.find({_id: d._id})) lCollection.insert(d);
-		})
+		const createdAt = new Date(`1-${new Date().getMonth() -1}-${new Date().getFullYear()}`);
+		if(options === 'json'){
+			collection.find({createdAt: {$gte: createdAt}}, {limit}).stream().pipe(JSONStream.stringify()).pipe(write);
+		}else{
+			collection.find({createdAt: {$gte: createdAt}}, {limit}).forEach(async d=> {
+			const doc = await lCollection.findOne({_id: d._id});
+			console.log(doc, 'da')
+			if(!doc) lCollection.insert(d);
+		});
+	}
 		}catch(err){
 			console.log(err)
 			lClient.close();
@@ -30,5 +38,18 @@ function main(url, localUrl) {
 		}
 	})
 	});
+	}else{
+		MongoClient.connect(localUrl, { useUnifiedTopology: true }, (err, client)=> {
+			if(err) console.log(err);
+			try{
+				const db = client.db(name);
+				const collection = db.collection(collectionName);
+				fs.readFile(process.cwd() + '/data.txt', (err, data)=> {
+					console.log(data.toString())
+					// collection.insertMany(data.toString());
+				})
+			}catch(error){console.log(error)}
+		});
+	}
 }
 main(url, localUrl);
